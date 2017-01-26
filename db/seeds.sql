61527426c44ec14997a6c1b52a50c9f07ecfedd9
Ldesir05 <ldesir5@mail.valenciacollege.edu>
2017-01-26 10:53:22 -0500

    added a few files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+-- inserting data into the database
+INSERT INTO `burger_db`.`burgers` (`burger_name`, `devoured`) VALUES ('chicken burger', '2');
+INSERT INTO `burger_db`.`burgers` (`burger_name`, `devoured`) VALUES ('steak burger', '3');
+INSERT INTO `burger_db`.`burgers` (`burger_name`, `devoured`) VALUES ('fish burger', '1');
+INSERT INTO `burger_db`.`burgers` (`burger_name`, `devoured`) VALUES ('classic burger', '4');
---
+++ server.js
@@ -0,0 +1,11 @@
+
+// requiring files
+var express = require('express');
+var bodyParse = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+
+
+
